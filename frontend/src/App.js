3d1764ebc90b461a83f857c73355288cba752b66
Snehal Mastud <smastud0@gmail.com>
2026-03-27 19:35:41 +0530

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import { PipelineToolbar } from './toolbar';
+import { PipelineUI } from './ui';
+import './styles/nodes.css';
+
+function App() {
+  return (
+    <div>
+      <PipelineToolbar />
+      <PipelineUI />
+    </div>
+  );
+}
+
+export default App;
